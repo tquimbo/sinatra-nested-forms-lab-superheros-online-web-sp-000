91f2d5c64a2f66095f48f5946bbed3af516d4b13
Tim Quimbo <tquimbo1@gmail.com>
2021-05-27 00:38:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get "/" do
+        erb :team
+    end
+
+    post '/teams' do
+        @team = params
+
+        erb :super_hero        
+    end
 
 end
